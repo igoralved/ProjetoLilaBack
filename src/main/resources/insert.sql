8d7f14d7e05537d5562e17d97c7ddba93e53492a
Gabriel Lima <gabriellimab@gmail.com>
2021-12-27 15:26:05 -0300

    set da senha do insert.sql

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_admin(id, senha) 
-VALUES('d1516d33-ff6f-4dc9-aedf-9316421096cb','senha secreta') ON CONFLICT(id) DO NOTHING;
+INSERT INTO tb_admin(id, senha)
+VALUES('d1516d33-ff6f-4dc9-aedf-9316421096cb','Lil@123') ON CONFLICT(id) DO NOTHING;
