ddfc51d009e6f0b519f425086267a9477d893777
Gabriel Lima <gabriellimab@gmail.com>
2021-12-28 18:13:49 -0300

    Refatoração dos códigos de Teste.

--- src/test/java/com/db/jogo/controller/LoginControllerTest.java
+++ src/test/java/com/db/jogo/controller/LoginControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.BDDMockito.given;
 import java.util.UUID;
 
 import com.db.jogo.model.Admin;
+import com.db.jogo.model.Autenticacao;
 import com.db.jogo.service.AdminService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -32,7 +33,7 @@ public class LoginControllerTest {
 	private AdminService adminService;
 
     @Test
-    public void testVerificaSenha() throws Exception{
+    public void testVerificaSenhaSucesso() throws Exception{
         Admin admin = new Admin();
         admin.setId(UUID.randomUUID());
         admin.setSenha("123");
@@ -43,11 +44,29 @@ public class LoginControllerTest {
             .content(asJsonString(admin))
             .accept(MediaType.APPLICATION_JSON_VALUE)  
             .contentType(MediaType.APPLICATION_JSON_VALUE))
-            .andExpect(content().string("true"))
+                .andExpect(jsonPath("$.logado").value("true"))
             .andExpect(status().isOk());
 
     }
 
+    @Test
+    public void testVerificaSenhaInvalida() throws Exception{
+        Admin admin = new Admin();
+        admin.setId(UUID.randomUUID());
+        admin.setSenha("123");
+        given(adminService.findBySenha("123")).willReturn(admin);
+
+        Admin adminRequest = new Admin();
+        adminRequest.setSenha("321");
+
+        this.mockMvc.perform(get("/login")
+                        .content(asJsonString(adminRequest))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.logado").value("false"))
+                .andExpect(status().isOk());
+
+    }
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
