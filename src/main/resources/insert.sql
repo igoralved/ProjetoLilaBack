c337aef07540aca4aa9bede4f9593bec33b98892
Gabriel Lima <gabriellimab@gmail.com>
2021-12-29 16:22:50 -0300

    testes gabriel

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -2,10 +2,10 @@ INSERT INTO admin(senha) VALUES('123')ON CONFLICT(id) DO NOTHING;
 INSERT INTO admin(senha) VALUES('456')ON CONFLICT(id) DO NOTHING;
 
 INSERT INTO public.baralho(id_codigo, descricao, titulo)
-	VALUES ("lila1", "primeiro Baralho", "baralho01") ON CONFLICT(id) DO NOTHING;
+	VALUES ('lila1', 'primeiro Baralho', 'baralho01') ON CONFLICT(id_codigo) DO NOTHING;
 
 INSERT INTO public.categoria_de_carta(nome)
-	VALUES ("visual") ON CONFLICT(id) DO NOTHING;
+	VALUES ('visual') ON CONFLICT(id) DO NOTHING;
 
 INSERT INTO public.tipo_carta(nome , bonus)
-	VALUES ("pessoa" , true) ON CONFLICT(id) DO NOTHING;
+	VALUES ('pessoa' , true) ON CONFLICT(id) DO NOTHING;
