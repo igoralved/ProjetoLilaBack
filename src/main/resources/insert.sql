16290c1dff95888cc425312badde2ac35af0d0dc
Stefany Eduarda <ostefany20@gmail.com>
2021-12-29 13:48:36 -0300

    correção do code review

--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -1,11 +1,11 @@
-INSERT INTO admin(senha) VALUES('123');
-INSERT INTO admin(senha) VALUES('456');
+INSERT INTO admin(senha) VALUES('123')ON CONFLICT(id) DO NOTHING;
+INSERT INTO admin(senha) VALUES('456')ON CONFLICT(id) DO NOTHING;
 
 INSERT INTO public.baralho(id_codigo, descricao, titulo)
-	VALUES ("lila1", "primeiro Baralho", "baralho01");
+	VALUES ("lila1", "primeiro Baralho", "baralho01") ON CONFLICT(id) DO NOTHING;
 
 INSERT INTO public.categoria_de_carta(nome)
-	VALUES ("visual");
+	VALUES ("visual") ON CONFLICT(id) DO NOTHING;
 
 INSERT INTO public.tipo_carta(nome , bonus)
-	VALUES ("pessoa" , true);
+	VALUES ("pessoa" , true) ON CONFLICT(id) DO NOTHING;
