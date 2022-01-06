39f2e423f4e4c4c020fae01125222326e5b1ee5e
AndreArjDev <andre.021arj@gmail.com>
2022-01-06 17:21:10 -0300

    teste controller

--- src/test/java/com/db/jogo/controller/JogadorControllerTest.java
+++ src/test/java/com/db/jogo/controller/JogadorControllerTest.java
@@ -1,12 +1,28 @@
 package com.db.jogo.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.db.jogo.model.Admin;
+import com.db.jogo.model.Jogador;
+import com.db.jogo.service.JogadorServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @WebAppConfiguration
@@ -14,10 +30,36 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebMvcTest(JogadorController.class)
 @DisplayName("Jogador Controller Teste")
 class JogadorControllerTest {
+	
+	@Autowired
+	MockMvc mockMvc;
 
+	@MockBean
+	JogadorServiceImpl jogadorService;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Teste do POST do Controller do Jogador")
+	public void testCriacaoJogador() throws Exception {
+		Jogador novoJogador = Jogador.builder()
+				.id(UUID.randomUUID())
+				.build();
+
+		ObjectMapper mapper = new ObjectMapper();
+		String novoJogadorComoJSON = mapper.writeValueAsString(novoJogador);
+		this.mockMvc.perform(post("/jogador")
+				.content(novoJogadorComoJSON)
+				.accept(MediaType.APPLICATION_JSON_VALUE)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isCreated());
+	}
+	
+	
+	
 	}
 
-}
+
+	
+
+
+
+
+	
---
+++ src/test/java/com/db/jogo/service/JogadorServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.db.jogo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.db.jogo.model.Jogador;
+
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Jogador Service Teste")
+class JogadorServiceImplTest {
+	@Mock
+    private JogadorServiceImpl jogadorService;
+	
+	Jogador jogador= Jogador.builder().build();
+
+    Optional<Jogador> jogadorOpt= Optional.of(jogador);
+
+	@Test
+	@DisplayName("Teste do GET do Controller do Jogador")
+	public void testBuscaJogador() throws Exception {
+	
+	    	
+	    	
+	    	UUID id = UUID.fromString("fd7b6723-77e2-4846-bd22-88df15ca150a"); 
+
+	        when(jogadorService.findById(id).thenReturn(jogadorOpt);
+
+	        assertEquals(jogadorOpt, jogadorService.findById(id));
+
+	    }
+}
