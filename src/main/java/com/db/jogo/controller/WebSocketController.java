dfbaaf99d3a8aa592cc8628ca87188f441d241d7
guismoreira <guilhermemoreira55@gmail.com>
2022-01-07 18:33:18 -0300

    implementação dos testes em WebSocketController

--- src/main/java/com/db/jogo/controller/WebSocketController.java
+++ src/main/java/com/db/jogo/controller/WebSocketController.java
@@ -7,13 +7,16 @@ import com.db.jogo.model.Sala;
 import com.db.jogo.service.SalaService;
 import com.db.jogo.service.WebSocketService;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @CrossOrigin
 @Slf4j
--- src/test/java/com/db/jogo/controller/WebSocketControllerTest.java
+++ src/test/java/com/db/jogo/controller/WebSocketControllerTest.java
@@ -1,36 +1,54 @@
-package com.db.jogo.service;
+package com.db.jogo.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.UUID;
 
-import com.db.jogo.exception.JogoInvalidoException;
+import com.db.jogo.dto.SalaRequest;
 import com.db.jogo.model.Baralho;
 import com.db.jogo.model.CartaDeObjetivo;
 import com.db.jogo.model.CartaDoJogo;
 import com.db.jogo.model.CartaInicio;
 import com.db.jogo.model.Jogador;
 import com.db.jogo.model.Sala;
+import com.db.jogo.service.SalaService;
+import com.db.jogo.service.WebSocketService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(MockitoExtension.class)
-public class WebSocketServiceTest {
 
-    @Mock
-    WebSocketService webSocketService;
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Websocket Controller Teste")
+public class WebSocketControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    @MockBean
+    WebSocketController webSocketController;
+
+    @Autowired
+    @MockBean
+    private WebSocketService webSocketService;
     
-    @Mock
+    @Autowired
+    @MockBean
     private SalaService salaService;
 
     CartaInicio cartaInicio = new CartaInicio();
@@ -39,6 +57,7 @@ public class WebSocketServiceTest {
     CartaDeObjetivo cartaObjetivo = new CartaDeObjetivo();
     Jogador jogador = new Jogador();
     Sala sala = new Sala();
+    SalaRequest salaRequest = new SalaRequest();
 
     @BeforeEach
     public void init(){
@@ -52,8 +71,8 @@ public class WebSocketServiceTest {
         carta.setCategoria("Visual");
         carta.setTexto("Deficiencia visual");
         carta.setFonte("Wikipedia");
-        carta.setValorCorGrande(0);
-        carta.setValorCorPequeno(0);
+        carta.setValorCorGrande(2);
+        carta.setValorCorPequeno(2);
         carta.setTipo("Ação");
 
         cartaObjetivo.setId(UUID.randomUUID());
@@ -75,11 +94,11 @@ public class WebSocketServiceTest {
 
         jogador.setId(UUID.randomUUID());
         jogador.setNome("Felipe");
-        jogador.setPontos(0);
-        jogador.setBonusCoracaoGra(0);
-        jogador.setBonusCoracaoPeq(0);
-        jogador.setCoracaoGra(0);
-        jogador.setCoracaoPeq(0);
+        jogador.setPontos(2);
+        jogador.setBonusCoracaoGra(3);
+        jogador.setBonusCoracaoPeq(2);
+        jogador.setCoracaoGra(5);
+        jogador.setCoracaoPeq(3);
         jogador.setListaDeCartas(new HashSet<>());
         jogador.adicionaCarta(carta);
         jogador.adicionaObjetivo(cartaObjetivo);
@@ -90,28 +109,34 @@ public class WebSocketServiceTest {
         sala.setStatusEnum(Sala.StatusEnum.NOVO);
         sala.setJogadores(new ArrayList<>());
         sala.adicionarJogador(jogador);
+        salaRequest.setHash("hashpraentrar");
+        salaRequest.setJogador(jogador);
     }
   
+    @Test
+    @DisplayName("Teste para conectar")
+    void testConectar() throws Exception{
+       
+        given(webSocketService.conectarJogo(salaRequest.getJogador(), salaRequest.getHash())).willReturn(Optional.of(sala));
+
+        ObjectMapper mapper = new ObjectMapper();
+        String newJogadorAsJSON = mapper.writeValueAsString(jogador);
+        this.mockMvc.perform(post("/conectar")
+                .content(newJogadorAsJSON)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
 
     @Test
-    @DisplayName("Teste para conectar ao jogo")
-    void testConectarJogo() {
-        sala.getHash();
-        try {
-            when(webSocketService.conectarJogo(jogador, sala.getHash())).thenReturn(Optional.of(sala));
-            assertEquals(sala, webSocketService.conectarJogo(jogador, sala.getHash()).get());
-        } catch (JogoInvalidoException e) {
-            // TODO Auto-generated catch block
-            fail("jogo inválido");
-        }
+    @DisplayName("Teste para iniciar jogo")
+    void testIniciarJogo() throws Exception{
 
     }
 
     @Test
-    @DisplayName("Teste para criar um jogo")
-    void testCriarJogo() {
-        
-        when(webSocketService.criarJogo(jogador)).thenReturn(sala);;
-        assertEquals(sala, webSocketService.criarJogo(jogador));
+    @DisplayName("Teste da jogada")
+    void testJogada() throws Exception{
+
     }
 }
