e342cd72ee97d3f369f92dbab79e276920152ef5
ricardo-DB <ricardo.guimaraes@dbserver.com.br>
2022-01-09 15:58:37 -0300

    Adicionando anotação no repository

--- src/main/java/com/db/jogo/repository/CartaInicioRepository.java
+++ src/main/java/com/db/jogo/repository/CartaInicioRepository.java
@@ -2,8 +2,10 @@ package com.db.jogo.repository;
 
 import com.db.jogo.model.CartaInicio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CartaInicioRepository extends JpaRepository<CartaInicio, UUID> {
 }
