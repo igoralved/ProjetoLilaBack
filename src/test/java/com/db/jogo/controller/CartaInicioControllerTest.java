53176f32cbf413e33a5334938f57335e51483021
igoralved <87991535+igoralved@users.noreply.github.com>
2022-01-13 15:27:45 -0300

    Add files via upload
    corrigindo nomes de variaveis

--- src/test/java/com/db/jogo/controller/CartaInicioControllerTest.java
+++ src/test/java/com/db/jogo/controller/CartaInicioControllerTest.java
@@ -1,99 +1,76 @@
 package com.db.jogo.controller;
 
-
 import com.db.jogo.model.CartaInicio;
-
-import com.db.jogo.service.CartaInicioServiceImpl;
+import com.db.jogo.service.CartaInicioService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 
-import java.util.Optional;
-import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.postgresql.hostchooser.HostRequirement.any;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebAppConfiguration
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@WebMvcTest(CartaInicioController.class)
 @DisplayName("Carta Inicio Controller Teste")
-public class CartaInicioControllerTest {
+class CartaInicioControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @MockBean
-    CartaInicioServiceImpl cartaInicioService;
-   
+    CartaInicioService cartaInicioService;
+
     String id = "d1516d33-ff6f-4dc9-aedf-9316421096cb";
     CartaInicio newCartaInicio = CartaInicio.builder()
-            .id(UUID.fromString(id))
-            .nome("nome")
-            .descricao("descrição")
+            .nome("testeNome")
+            .descricao("testeDescrição")
             .build();
-
-
-
-
+    
     @Test
-    @DisplayName("Teste do POST/Sucesso do Controller Carta Inicio")
-    public void deveRetornarSucesso_QuandoCriarCartaInicio() throws Exception {
+    @DisplayName("Teste do POST do Controller da Carta Inicio")
+    public void testCriacaoCartaInicio() throws Exception {
+        CartaInicio newCartaInicio = CartaInicio.builder()
+                .nome("testeNome")
+                .descricao("testeDescrição")
 
-        when(cartaInicioService.saveCartaInicio(any(CartaInicio.class))).thenReturn(newCartaInicio);
-
+                .build();
 
         ObjectMapper mapper = new ObjectMapper();
-        String cartaDoJogoJSON = mapper.writeValueAsString(newCartaInicio);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartainicio").content(cartaDoJogoJSON)
-                .accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isCreated()).andExpect(MockMvcResultMatchers.content().json(cartaDoJogoJSON));
+        String newcartaInicioAsJSON = mapper.writeValueAsString(newCartaInicio);
+        this.mockMvc.perform(post("/cartaInicio").content(newcartaInicioAsJSON).accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isCreated());
     }
 
-    @Test
-    @DisplayName("Teste do POST/Error do Controller da Carta Inicio")
-    public void deveRetornarErro_QuandoCriarJogadorInvalido() throws Exception {
-
-
-        when(cartaInicioService.saveCartaInicio(any(CartaInicio.class))).thenReturn(newCartaInicio);
-        mockMvc.perform(MockMvcRequestBuilders.post("/cartainicio").accept(MediaType.APPLICATION_JSON_VALUE)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isBadRequest());
-
-    }
     @Test
     @DisplayName("Teste do GET do Controller Carta Inicio")
     public void deveRetornarSucesso_QuandoBuscar() throws Exception {
 
-        given(cartaInicioService.findById(newCartaInicio.getId())).willReturn(Optional.of(newCartaInicio));
+
+        given(cartaInicioService.findAllCartaInicio()).willReturn(cartaInicioService.findAllCartaInicio());
 
         ObjectMapper mapper = new ObjectMapper();
-        String cartaInicioJSON = mapper.writeValueAsString(newCartaInicio);
+        String cartaInicioComoJSON = mapper.writeValueAsString(newCartaInicio);
 
-        mockMvc.perform(get("/cartainicio/" + newCartaInicio.getId()).content(cartaInicioJSON)
+        mockMvc.perform(get("/cartaInicio" + cartaInicioService.findAllCartaInicio()).content(cartaInicioComoJSON)
                 .accept(MediaType.APPLICATION_JSON_VALUE).contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk()).andExpect(MockMvcResultMatchers.content().json(cartaInicioJSON));
+                .andExpect(status().isOk()).andExpect((ResultMatcher) content().json(cartaInicioComoJSON));
+
 
     }
-
 }
