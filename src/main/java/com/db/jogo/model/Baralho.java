38300e56f36ae50c2ed0776292e0c38ad221d98e
Gabriel Lima <gabriellimab@gmail.com>
2022-01-14 11:20:09 -0300

    cleanup fixes

--- src/main/java/com/db/jogo/model/Baralho.java
+++ src/main/java/com/db/jogo/model/Baralho.java
@@ -4,22 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import javax.annotation.Generated;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.persistence.OneToMany;
 
 import org.springframework.lang.NonNull;
-
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
