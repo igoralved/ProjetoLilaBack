f6d62d769cf440614ee8f0aa01f1fd932018cc67
Stefany Eduarda <ostefany20@gmail.com>
2022-01-25 15:04:50 -0300

    ajustes de nomeclatura

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -76,7 +76,7 @@ create table baralho_cartainicio (
 create table baralho_cartaobjetivo (
     baralho_id uuid not null,
     cartaobjetivo_id uuid not null,
-     CONSTRAINT FK_cartaobjtivo_id FOREIGN KEY (cartaobjetivo_id)
+     CONSTRAINT FK_cartaobjetivo_id FOREIGN KEY (cartaobjetivo_id)
      REFERENCES carta_objetivo (id),
      CONSTRAINT FK_baralho_id FOREIGN KEY (baralho_id)
      REFERENCES baralho (id));
